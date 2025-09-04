37744b52264736390b334356234e22d425b9c11d
d0lwl0b <208416493+d0lwl0b@users.noreply.github.com>
2025-09-04 12:30:46 +0800

    fix: adjust gas price condition in main loop to refine sampling logic

--- basescan.py
+++ basescan.py
@@ -161,7 +161,7 @@ def main(loop=False, interval=60, toml_path=None, min_factor=1.05, max_factor=1.
                 use_gas = None
                 if sampled_price > mid_mean * factor:
                     use_gas = sampled_price
-                elif sampled_price < mid_mean * 0.62:
+                elif sampled_price < mid_mean * 1.618:
                     use_gas = low_mean if low_mean else min_gas
                     use_gas = use_gas * factor
                 else:
