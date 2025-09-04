d9d90f2dc6dc463ad3c487548d402e275cf849e6
d0lwl0b <208416493+d0lwl0b@users.noreply.github.com>
2025-09-05 02:27:45 +0800

    fix: update score matching logic in main function for accurate gas price determination

--- basescan.py
+++ basescan.py
@@ -278,14 +278,14 @@ def main(loop=False, interval=60, toml_path=None, factor=1.07, max_gas=int(3e9),
                 score = sum([tuple_to_bitmask(quantile_to_tuple(q)) for q in analyze_data.latest3_tags])
                 LogPrint.info(f"[RANDOM] score: {score} from latest3 tags: {[q.value if q else None for q in analyze_data.latest3_tags]}")
                 match score:
-                    case 3 | 4 | 5:  # low
+                    case 3 | 4 | 5 | 6:  # low
                         mine_area = GasQuantile.HIGH
-                    case 6 | 7 | 8 | 9:
+                    case 7 | 8:
                         mode = ExecMode.UP
                         gas_price = analyze_data.low.min
                         last_gas_price = gas_price
                         continue
-                    case 10 | 11 | 12:  # high
+                    case 9 | 10 | 11 | 12:  # high
                         mine_area = GasQuantile.LOW
 
                 for _, price in area_data[MONITOR_ADDRESSES[0]][mine_area]:
